a8bd7ae7a4e10a2082bd681a6e41c5ac62a6fe96
sd529576 <sd529576@gmail.com>
2022-08-16 15:44:20 -0400

    adding existing files

---
+++ server.js
@@ -0,0 +1,29 @@
+/* eslint-disable no-console */
+import express from 'express';
+import db from './server_files/database/initializeDB.js';
+import apiRoutes from './server_files/routes/apiRoutes.js';
+
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+const staticFolder = 'public';
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static(staticFolder));
+app.use('/api', apiRoutes);
+
+async function bootServer() {
+  try {
+    // const mysql = await db.sequelizeDB;
+    // await mysql.sync();
+    app.listen(PORT, () => {
+      console.log(`Listening on: http//localhost:${PORT}`);
+    });
+  } catch (err) {
+    console.error(err);
+  }
+}
+
+bootServer();
---
+++ server_files/database/config.js
@@ -0,0 +1,23 @@
+export default {
+  development: {
+    username: 'ba10c5f2bce0c8',
+    password: 'dee927511e3c82f',
+    database: 'heroku_84811f648356c7f',
+    host: 'us-cdbr-east-05.cleardb.net',
+    dialect: 'mysql'
+  },
+  test: {
+    username: 'ba10c5f2bce0c8',
+    password: 'dee927511e3c82f',
+    database: 'heroku_84811f648356c7f',
+    host: 'us-cdbr-east-05.cleardb.net',
+    dialect: 'mysql'
+  },
+  production: {
+    username: 'ba10c5f2bce0c8',
+    password: 'dee927511e3c82f',
+    database: 'heroku_84811f648356c7f',
+    host: 'us-cdbr-east-05.cleardb.net',
+    dialect: 'mysql'
+  }
+};
